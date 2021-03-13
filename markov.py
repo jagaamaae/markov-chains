4f1ce1d856a9cd2b72c18560757d2337411fa0ac
vagrant <erde0011@umn.edu>
2021-03-13 07:01:18 +0000

    final

--- markov.py
+++ markov.py
@@ -1,4 +1,6 @@
 """Generate Markov text from text files."""
+
+import sys
 from random import choice
 
 def open_and_read_file(file_path):
@@ -8,12 +10,8 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
    
-    data= open(file_path)#object
-    data=data.read()#turned to a giant sting
-    #lines = data.split()
-    
-    #for text_string in data:
-    #   print(text_string)
+    data = open(file_path).read()#object
+    # data=data.read()#turned to a giant sting
     return data
 
 
@@ -49,44 +47,36 @@ def make_chains(text_string):
     #print(words)
     
     for i in range(len(words)-2):# i is a counter
-        key = ( words[i], words[i + 1] ) #words at 1, 2 at become keys as a tuple
-        value = words[i + 2] #word 3 is a value if a list
+        # key = ( words[i], words[i + 1] ) #words at 1, 2 at become keys as a tuple
+        # value = words[i + 2] #word 3 is a value if a list
         #.setdefault.creates with an empty list, then append to the empty list
         #if key not in chains:
         #    chains[key]=[]
-        chains.setdefault(key, []).append(value)
         #chains[key].append(value)
-        chains.setdefault(key, []).append(value)
+        chains.setdefault((words[i],words[i+1]), []).append(words[i+2])
         #chains[key].append(value)
     print(type(chains))
     return chains
+    print(chains)
 
 import random
 
 def make_text(chains):
     """Return text from chains."""
-    #A link in our case is a key from our dictionary 
-    # and a random word from the list of words that follow it. 
-    #Put that link in some kind of container 
-    # (the skeleton file suggests adding each word 
-    # to a list and joining the list into a string at the end).
-    #  Once we have our first link, we can add another to it, 
-    # and we can repeat the following process to add more:
-    # make a new key out of the second word in the first key and the random word you pulled out from the list of words
-   #look up that new key in the dictionary, and pull a new random word out of the new list.
     key = choice(list(chains.keys()))#key is a list with all the random keys
     word = choice(chains[key]) #random choice of key from teh dict
-    container = [key[0], key[1]] #container is a list and contains 1st or 2nd keys
+    words = [key[0], key[1]] #container is a list and contains 1st or 2nd keys
 
 
     while word is not None:
         
         key = (key[1], word)#make a new key out of the second word in the first key and the random word 
-        container. append(word)
+        words. append(word)
         word = choice(chains[key]) # random word
-        return ' '.join(container)
+        return ' '.join(words)
     
-input_path = 'green-eggs.txt'
+input_path = 'gettysburg.txt'
+#input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
